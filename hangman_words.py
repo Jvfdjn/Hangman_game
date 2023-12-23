451ba2602bf29ff2821bd1593248cc6af7b42e6b
SHIV RAJ SINGH <144125626+Jvfdjn@users.noreply.github.com>
2023-12-23 21:58:05 +0530

    Add files via upload

---
+++ hangman_words.py
@@ -0,0 +1,23 @@
+hangman_words = [
+    "python",
+    "hangman",
+    "programming",
+    "computer",
+    "developer",
+    "learning",
+    "challenge",
+    "code",
+    "debugging",
+    "algorithm",
+    "loop",
+    "function",
+    "variable",
+    "array",
+    "list",
+    "dictionary",
+    "condition",
+    "iteration",
+    "encapsulation",
+    "inheritance",
+    "polymorphism"
+]
